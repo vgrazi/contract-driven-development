36f63c625c170a4f91d235907080ebef0b7129e9
Victor Grazi <vgrazi@gmail.com>
2019-09-16 21:29:05 -0400

    removed dates from provider side responses

--- spring-cloud-contracts-provider/src/main/java/com/vgrazi/presentations/springcloudcontractsprovider/gateway/CreditIncreaseResponse.java
+++ spring-cloud-contracts-provider/src/main/java/com/vgrazi/presentations/springcloudcontractsprovider/gateway/CreditIncreaseResponse.java
@@ -1,26 +1,12 @@
 package com.vgrazi.presentations.springcloudcontractsprovider.gateway;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class CreditIncreaseResponse {
-    @Getter @Setter
     private int clientId;
-    @Getter @Setter
     private double increaseAmount;
-
-    @Getter @Setter
     private String denialReason;
-
-    @Getter private String date;
-
-    public CreditIncreaseResponse(int clientId, double increaseAmount, String date) {
-        this.clientId = clientId;
-        this.increaseAmount = increaseAmount;
-        this.date = date;
-    }
 }
