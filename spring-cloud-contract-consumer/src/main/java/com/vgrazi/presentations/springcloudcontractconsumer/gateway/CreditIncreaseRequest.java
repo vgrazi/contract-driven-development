c908d7c7478e27a3e85dd8549f8da70ec3cc45df
Victor J Grazi <vgrazi@gmail.com>
2019-11-23 21:24:08 -0500

    Update CreditIncreaseRequest.java

--- spring-cloud-contract-consumer/src/main/java/com/vgrazi/presentations/springcloudcontractconsumer/gateway/CreditIncreaseRequest.java
+++ spring-cloud-contract-consumer/src/main/java/com/vgrazi/presentations/springcloudcontractconsumer/gateway/CreditIncreaseRequest.java
@@ -2,10 +2,7 @@ package com.vgrazi.presentations.springcloudcontractconsumer.gateway;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class CreditIncreaseRequest {
